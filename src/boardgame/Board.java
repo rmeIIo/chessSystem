9bae63b17e3e07ac9df0a15b05c180e36525af46
rmeiio <j.rogercorreamello@gmail.com>
2024-06-26 08:42:07 -0300

    ChessException and ChessPosition

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -24,17 +24,17 @@ public class Board {
     }
 
     public Piece piece(int row, int column) {
-        if(!positionExists(row, column)) {
-            throw new BoardException("Position not on the board");
-        }
+        //if(!positionExists(row, column)) {
+        //    throw new BoardException("Position not on the board");
+        //}
 
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
-        if(!positionExists(position)) {
-            throw new BoardException("Position not on the board");
-        }
+        //if(!positionExists(position)) {
+        //    throw new BoardException("Position not on the board");
+        //}
         
         return pieces[position.getRow()][position.getColumn()];
     }
@@ -57,9 +57,9 @@ public class Board {
     }
 
     public boolean thereIsAPiece(Position position) {
-        if(!positionExists(position)) {
-            throw new BoardException("Position not on the board");
-        }
+        //if(!positionExists(position)) {
+        //    throw new BoardException("Position not on the board");
+        //}
 
         return piece(position) != null;
     }
